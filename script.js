aed8ef167dd1da4512aa97ba4d7645cd43a23406
AzizBooker <bookeraziz@gmail.com>
2021-06-22 14:27:34 -0400

    added modal and hamburger icon change

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ tl
     x:150,
    duration:.75
 })
+
+
+body=document.querySelector('body')
+openHamburger=document.querySelector("#hamburger_menu_open");
+closeHamburger=document.querySelector('#hamburger_menu_close');
+menuModal=document.querySelector('.menu_modal')
+modalOverlay=document.querySelector('.overlay')
+
+openHamburger.addEventListener('click',ev=>{
+    console.log('click')
+    openHamburger.style.display="none"
+    closeHamburger.style.display="inline-block"
+    menuModal.style.display="block"
+    body.style.overflow="hidden"
+  //  modalOverlay.style.display="block"
+
+})
+closeHamburger.addEventListener("click",ev=>{
+    console.log('click')
+    openHamburger.style.display="inline-block"
+    closeHamburger.style.display="none"
+    menuModal.style.display="none"
+    body.style.overflow="auto"
+  //  modalOverlay.style.display="none"
+})
